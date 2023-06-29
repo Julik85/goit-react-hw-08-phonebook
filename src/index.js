52cfb9ba79c425efe9f43280cccca219019cba67
Julik85 <mironuk@ukr.net>
2023-06-30 01:43:01 +0300

    fix 2

--- src/index.js
+++ src/index.js
@@ -7,26 +7,14 @@ import { persistor, store } from 'redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
 
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//   <Provider store={store}>
-//     <PersistGate loading={null} persistor={persistor}>   
-//       <BrowserRouter basename="/phonebook">
-//         <App />
-//       </BrowserRouter>
-//     </PersistGate>
-//   </Provider>
-// </React.StrictMode>
-// );
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/goit-react-hw-08-phonebook">
-          <App />
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>   
+      <BrowserRouter >
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
+</React.StrictMode>
 );
--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://6495c1b1b08e17c91792a2bc.mockapi.io';
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
 
 export const fetchContacts = createAsyncThunk(
     'contacts/fetchAll',
